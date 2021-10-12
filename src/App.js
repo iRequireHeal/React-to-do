1f4bed34e052cd66228fc6a899dbabc829f02365
iRequireHeal <i1ovdude@gmail.com>
2021-10-12 21:31:30 +0500

    finished filters

--- src/App.js
+++ src/App.js
@@ -9,7 +9,12 @@ function App() {
         {id: 1, name: "a mojet ti suraka?", isReady: true},
         {id: 2, name: "ili ti monako?", isReady: false}
     ]);
-    const [taskFilter, setTaskFilter] = useState(taskList)
+
+    const SHOW_ALL = "SHOW_ALL"
+    const COMPLETED_TASKS = "COMPLETED_TASKS"
+    const ACTIVE_TASKS = "ACTIVE_TASKS"
+
+    const [taskFilter, setTaskFilter] = useState(SHOW_ALL)
 
     const toggleReady = (id) => {
         setTaskList(taskList.map(selectedTask => selectedTask.id === id ? {
@@ -30,19 +35,21 @@ function App() {
         }])
     }
 
-    const[tasks, setTasks] = useState(taskList.map(task => <Task task={task} deleteTask={deleteTask} toggleReady={toggleReady}/>))
+    const [tasks, setTasks] = useState(taskList.map(task => <Task task={task} deleteTask={deleteTask}
+                                                                  toggleReady={toggleReady}/>))
 
-    const compleatedFilter = () => {
-        setTasks(taskList.map(task => task.isReady === true ?
-            <Task task={task} deleteTask={deleteTask} toggleReady={toggleReady}/> : null))
-    }
-
-    const allFilter = () => {
-        setTasks(taskList.map(task => <Task task={task} deleteTask={deleteTask} toggleReady={toggleReady}/>))
-    }
-    const activeFilter = () => {
-        setTasks(taskList.map(task => task.isReady === false ?
-            <Task task={task} deleteTask={deleteTask} toggleReady={toggleReady}/> : null))
+    const renderTasks = () => {
+        switch (taskFilter) {
+            case SHOW_ALL:
+                return taskList.map(task =>  <Task task={task} deleteTask={deleteTask} toggleReady={toggleReady}/>)
+                break
+            case COMPLETED_TASKS:
+                return  taskList.filter(task => task.isReady === true).map(task => <Task task={task} deleteTask={deleteTask} toggleReady={toggleReady}/>)
+                break
+            case ACTIVE_TASKS:
+                return  taskList.filter(task => task.isReady === false).map(task => <Task task={task} deleteTask={deleteTask} toggleReady={toggleReady}/>)
+                break
+        }
     }
 
     return (
@@ -52,11 +59,11 @@ function App() {
                 <div className="inputButtons">
                     <input className="taskInput" value={taskName} onChange={(event) => setName(event.target.value)}/>
                     <button className="addButton" onClick={() => addTask()}>ADD</button>
-                    <button className="buttons" onClick={() =>allFilter()}>ALL</button>
-                    <button className="buttons" onClick={() =>compleatedFilter()}>Compleated()</button>
-                    <button className="buttons" onClick={() =>activeFilter()}>Active</button>
+                    <button className="buttons" onClick={() =>setTaskFilter(SHOW_ALL)}>ALL</button>
+                    <button className="buttons" onClick={() =>setTaskFilter(COMPLETED_TASKS)}>Compleated()</button>
+                    <button className="buttons" onClick={() =>setTaskFilter(ACTIVE_TASKS)}>Active</button>
                 </div>
-                <div>{tasks}</div>
+                <div>{renderTasks()}</div>
             </div>
         </div>
     )
